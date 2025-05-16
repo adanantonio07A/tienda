800e33c276d96ceeba2d7b57c6109fcb8e99b5c4
Adan Alonso <aalonso@centro-tecnologico.com>
2025-05-16 02:23:54 -0600

    Add mysql to jenkins-docker container.
        These changes will add the mysql app to handle the database.
    
        Signed-off-by: Adan Alonso <adanantonio07@gmail.com>

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://emdep:3md3p@localhost:3306/tienda_db"
+DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/tienda_db"
 
 try:
     engine = create_engine(DATABASE_URL)
