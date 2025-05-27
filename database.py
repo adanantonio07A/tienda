db7c95d5a88783eed6db1c8d2a33458113476d56
Adan Alonso <aalonso@centro-tecnologico.com>
2025-05-26 21:52:27 -0600

    Adding Docker Baseline.
        These changes will add the features to use docker, launch the
        required containers and interact with the database.
    
        Signed-off-by: Adan Alonso <Adanantonio07@gmail.com>

--- database.py
+++ database.py
@@ -1,7 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/tienda_db"
+import os
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "tienda_db")
+DB_USER = os.getenv("DB_USER", "tiendauser")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "tiendapass")
+
+DATABASE_URL = (
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+#DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/tienda_db"
 
 try:
     engine = create_engine(DATABASE_URL)
